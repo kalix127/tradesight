3a294c3e6b86841354e8f81dd5b5beb01e4d20f5
Gianluca Iavicoli <gianluca.iavicoli04@gmail.com>
2025-12-14 22:41:44 +0100

    remove: Remove Python API and related files from web-ui

--- web-ui/app/app.config.ts
+++
@@ -1,6 +0,0 @@
-export default defineAppConfig({
-  projectName: "TR Converter",
-  contactEmail: "info@gianlucaiavicoli.dev",
-  githubUsername: "kalix127",
-  githubRepositoryUrl: "https://github.com/kalix127/trade-republic-pdf-to-csv",
-});
